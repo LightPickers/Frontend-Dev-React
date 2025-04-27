6e136a6b9b24c4363c4189751218bb536fb53a44
zxlee0114 <napoleon.lee0114@gmail.com>
2025-04-27 22:05:02 +0800

    feat: 創建 Demo 登入檔案

--- src/LightPickersApp.jsx
+++ src/LightPickersApp.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 
 import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
+import DemoRegister from "./DemoRegister";
 
 function LightPickersApp() {
   return (
     <>
       <Header />
+      <DemoRegister />
       <Outlet />
       <Footer />
     </>
