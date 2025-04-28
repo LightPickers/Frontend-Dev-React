4d702677de620fda64a17825143dc3af42e6e7ad
zxlee0114 <napoleon.lee0114@gmail.com>
2025-04-29 02:44:09 +0800

    fix: 樣式設定新增與修改

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,29 +1,29 @@
 import classNames from "classnames";
 import PropTypes from "prop-types";
 
-function Button({ text, size = "medium", className, disabled = false, onClick }) {
+function PrimaryButton({ children, size = "medium", className, disabled = false, onClick }) {
   const classes = classNames("btn-custom", size, className);
   return (
     <>
       <button className={classes} disabled={disabled} onClick={onClick}>
-        {text}
+        {children}
       </button>
     </>
   );
 }
 
-Button.propTypes = {
-  text: PropTypes.string.isRequired, // 必填、字串
+PrimaryButton.propTypes = {
+  children: PropTypes.string.isRequired, // 必填、字串
   className: PropTypes.string,
   onClick: PropTypes.func, // 函式
   size: PropTypes.oneOf(["small", "medium", "large", "cta"]), // 四值擇一
   disabled: PropTypes.bool, // 布林值
 };
 
-Button.defaultProps = {
+PrimaryButton.defaultProps = {
   size: "medium",
   disabled: false,
   onClick: () => {},
 };
 
-export default Button;
+export default PrimaryButton;
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,16 +1,15 @@
-import Button from "@components/Button";
+import PrimaryButton from "@components/Button";
 
 function Header() {
   return (
     <>
-      <p className="text-primary-600">這是 header</p>
-      <button className="btn-custom cta" type="button">
-        我是按鈕
-      </button>
-      <Button text={"cta"} size="cta" />
-      <Button text={"large"} size="large" />
-      <Button text={"medium"} size="medium" />
-      <Button text={"small"} size="small" />
+      <h1>標題一</h1>
+      <h2>標題二</h2>
+      <h3>標題三</h3>
+      <h4>標題四</h4>
+      <h5>標題五</h5>
+      <p className="text-primary-600 fs-3">這是 header</p>
+      <PrimaryButton>我是按鈕</PrimaryButton>
     </>
   );
 }
