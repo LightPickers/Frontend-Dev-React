25fa5fc7c390a437706e60a2602b45fb3c1db935
zxlee0114 <napoleon.lee0114@gmail.com>
2025-05-01 22:31:00 +0800

    refactor: 用 RTK query 來撰寫登入邏輯

--- src/pages/AuthPages/LoginPage.jsx
+++ src/pages/AuthPages/LoginPage.jsx
@@ -1,29 +1,27 @@
 import { useForm } from "react-hook-form";
-import axios from "axios";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import BtnPrimary from "@/components/Button";
+import BtnPrimary from "@components/Button";
+import { useLoginMutation } from "@features/auth/authApi";
+import { loginSchema } from "@schemas/users/loginSchema";
+import loginAndRedirect from "@features/auth/loginAndRedirect";
 
 function LoginPage() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+  const [login, { isLoading, error }] = useLoginMutation();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const API_BASE = import.meta.env.VITE_API_BASE;
-
-  const onSubmit = async data => {
-    try {
-      const res = await axios.post(`${API_BASE}/login`, data);
-      const { token } = res.data.data;
-      const { id: userId } = jwtDecode(token);
-      localStorage.setItem("token", token);
-      console.log(localStorage.getItem("token"));
-      navigate(`/account/${userId}/settings`);
-    } catch (error) {
-      console.error(error);
-      alert("登入失敗");
-    }
+  const onSubmit = async loginData => {
+    await loginAndRedirect({ login, dispatch, navigate, loginData }); // hook 只能在元件存變數後傳給通用函式
   };
-
   return (
     <div className="container py-5">
       <div className="row">
@@ -32,34 +30,46 @@ function LoginPage() {
             <div className="card-body">
               <h2 className="card-title text-center mb-4">會員登入</h2>
               <form onSubmit={handleSubmit(onSubmit)}>
+                {/* email */}
                 <div className="mb-3">
-                  <label htmlFor="registerEmail" className="form-label">
+                  <label htmlFor="loginEmail" className="form-label">
                     Email
                   </label>
                   <input
                     type="email"
-                    className="form-control"
-                    id="registerEmail"
+                    className={`form-control ${errors.email ? "is-invalid" : ""}`}
+                    id="loginEmail"
                     placeholder="請輸入 Email"
                     {...register("email")}
                   />
+                  {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
                 </div>
 
+                {/* 密碼 */}
                 <div className="mb-3">
-                  <label htmlFor="registerPassword" className="form-label">
+                  <label htmlFor="loginPassword" className="form-label">
                     密碼
                   </label>
                   <input
                     type="password"
-                    className="form-control"
-                    id="registerPassword"
+                    id="loginPassword"
+                    className={`form-control ${errors.password ? "is-invalid" : ""}`}
                     placeholder="請輸入密碼"
                     {...register("password")}
                   />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password.message}</div>
+                  )}
                 </div>
+                {/* {error?.data?.message && (
+                  <div className="alert alert-danger mt-3" role="alert">
+                    {error.data.message}
+                  </div>
+                )} */}
 
-                <BtnPrimary size="large" className="w-100">
-                  登入
+                {/* 提交按鈕 */}
+                <BtnPrimary size="large" className="w-100" disabled={isLoading}>
+                  {isLoading ? "登入中..." : "登入"}
                 </BtnPrimary>
               </form>
             </div>
