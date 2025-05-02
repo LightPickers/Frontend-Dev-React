e3f25731cca60e988ec324cc225309d74a6dcb6c
zxlee0114 <napoleon.lee0114@gmail.com>
2025-05-02 22:37:55 +0800

    feat: 將取得行政區的方法改為寫死的方式（遠端取得不太穩定）

--- src/hooks/useTaiwanDistricts.jsx
+++ src/hooks/useTaiwanDistricts.jsx
@@ -1,30 +1,46 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
-function useTaiwanDistricts() {
+import { taiwanDistricts } from "@/data/taiwanDistricts";
+
+export default function useTaiwanDistricts() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    async function fetchDistricts() {
-      try {
-        const res = await axios.get(
-          "https://gist.githubusercontent.com/abc873693/2804e64324eaaf26515281710e1792df/raw/taiwan_districts.json"
-        );
-        setData(res.data);
-      } catch (err) {
-        console.error("載入失敗：", err);
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchDistricts();
+    setData(taiwanDistricts);
+    setLoading(false);
   }, []);
 
   return { data, loading, error };
 }
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
-export default useTaiwanDistricts;
+// function useTaiwanDistricts() {
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     async function fetchDistricts() {
+//       try {
+//         const res = await axios.get(
+//           "https://gist.githubusercontent.com/abc873693/2804e64324eaaf26515281710e1792df/raw/taiwan_districts.json"
+//         );
+//         setData(res.data);
+//       } catch (err) {
+//         console.error("載入失敗：", err);
+//         setError(err);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchDistricts();
+//   }, []);
+
+//   return { data, loading, error };
+// }
+
+// export default useTaiwanDistricts;
