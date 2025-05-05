4d2d4af48279c6c618e75097bd65ce69a74e0a04
zxlee0114 <napoleon.lee0114@gmail.com>
2025-05-05 22:34:02 +0800

    style: 刪除不必要的檔案

--- src/LightPickersApp.jsx
+++ src/LightPickersApp.jsx
@@ -1,12 +1,8 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 
 import Header from "@layouts/Header";
 import Footer from "@layouts/Footer";
-import StyleSettings from "@/layouts/StyleSettings";
-import { useVerifyAuthQuery } from "@features/users/userApi";
-import { logout, setCredentials } from "@features/auth/authSlice";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -19,23 +15,11 @@ function ScrollToTop() {
 }
 
 function LightPickersApp() {
-  // const dispatch = useDispatch();
-  // const { data, error, isSuccess } = useVerifyAuthQuery();
-
-  // useEffect(() => {
-  //   if (isSuccess && data?.user) {
-  //     dispatch(setCredentials({ user: data.user, token: null }));
-  //   } else if (error) {
-  //     dispatch(logout());
-  //   }
-  // }, [isSuccess, error, data, dispatch]);
-
   return (
     <>
       <ScrollToTop />
       <Header />
       <Outlet />
-      {/* <StyleSettings /> */}
       <Footer />
     </>
   );
