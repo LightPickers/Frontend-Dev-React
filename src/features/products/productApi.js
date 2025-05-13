09fc4c4975c6791d395dad5df2dbe1e21e127950
zxlee0114 <napoleon.lee0114@gmail.com>
2025-05-13 09:54:59 +0800

    feat(productApi): 新增產品 API，並註冊到 store

--- src/features/products/productApi.js
+++ src/features/products/productApi.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// 前台產品相關 API
 export const productApi = createApi({
   reducerPath: "productApi",
   baseQuery: fetchBaseQuery({
@@ -12,5 +13,32 @@ export const productApi = createApi({
       query: () => "/products",
       providesTags: ["Product"],
     }),
+    // 取得精選產品
+    getFeaturedProducts: builder.query({
+      query: () => "/products/featured",
+      providesTags: ["Product"],
+    }),
+    // 取得最新產品
+    getLatestProducts: builder.query({
+      query: () => "/products/latest?sort=latest",
+      providesTags: ["Product"],
+    }),
+    // 取得單一產品
+    getProductById: builder.query({
+      query: productId => `/products/${productId}`,
+      providesTags: (result, error, id) => [{ type: "Product", id }],
+    }),
   }),
 });
+
+export const {
+  useGetFeaturedProductsQuery,
+  useGetLatestProductsQuery,
+  useGetProductByIdQuery,
+  useGetProductsQuery,
+  useLazyGetFeaturedProductsQuery,
+  useLazyGetLatestProductsQuery,
+  useLazyGetProductByIdQuery,
+  useLazyGetProductsQuery,
+  usePrefetch,
+} = productApi;
--- src/store.js
+++ src/store.js
@@ -2,13 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "@features/auth/authSlice";
 import { userApi } from "@features/users/userApi";
+import { productApi } from "@/features/products/productApi";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     [userApi.reducerPath]: userApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(userApi.middleware),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(userApi.middleware).concat(productApi.middleware),
 });
 
 export default store;
