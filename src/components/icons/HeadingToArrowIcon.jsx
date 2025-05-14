060815fd582cc5a6f7dc32d02bd36f7f30f6d1b6
zxlee0114 <napoleon.lee0114@gmail.com>
2025-05-14 16:18:11 +0800

    fix: 修正 SVG  icon JSX 語法錯誤問題

--- src/components/icons/HeadingToArrowIcon.jsx
+++ src/components/icons/HeadingToArrowIcon.jsx
@@ -17,8 +17,8 @@ function HeadingToArrowIcon({ size = 24, strokeWidth = 0, title, className, styl
     >
       <title>{title}</title>
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M36.906 9L43.8273 15.105C43.9844 15.2435 44.041 15.4681 43.9693 15.6677C43.8975 15.8674 43.7125 16 43.5056 16H0V14.9825H42.1691L36.2625 9.77257L36.906 9Z"
         fill="currentColor"
       />
