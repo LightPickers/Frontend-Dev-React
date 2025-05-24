2d22f64ded64f63961471bc10cf70ed95ec5884a
zxlee0114 <napoleon.lee0114@gmail.com>
2025-05-24 10:13:11 +0800

    chore (CartIconToggler): 註解 log、更換變數名稱

--- src/components/CartIconToggler.jsx
+++ src/components/CartIconToggler.jsx
@@ -17,38 +17,35 @@ function CartIconToggler({ productId }) {
   const [deleteCartProduct, { isLoading: isRemovingFromCart }] = useDeleteCartProductMutation();
   const userId = useDecodedId();
   const { data: cartList } = useGetCartQuery(userId);
-  console.log({ cartList, productId });
+  // console.log({ cartList, productId });
   const requireAuth = useRequireAuth();
-  const isWaitingToBuy = cartList?.data.items.some(item => item.id === productId);
+  const isInCart = cartList?.data.items.some(item => item.id === productId);
   const isLoading = isAddingToCart || isRemovingFromCart;
 
   const handleToggleIcon = async () => {
     try {
-      if (isWaitingToBuy) {
+      if (isInCart) {
         const targetIndex = cartList?.data.items.findIndex(item => item.id === productId);
         const targetId = cartList?.data.items[targetIndex].id;
         await deleteCartProduct(targetId).unwrap();
       } else {
         await addToCart(productId);
       }
-      toast.success(isWaitingToBuy ? "已在購物車移除" : "已加入購物車");
+      toast.success(isInCart ? "已在購物車移除" : "已加入購物車");
     } catch (error) {
       toast.error(getApiErrorMessage(error));
     }
   };
   return (
     <IconActionButton
-      isActive={isWaitingToBuy}
+      isActive={isInCart}
       isLoading={isLoading}
       icon={<CartIcon title="加入購物車" strokeWidth={1} />}
       activeIcon={<CartFilledIcon title="在購物車移除商品" strokeWidth={1} />}
       onClick={() => requireAuth(handleToggleIcon)}
-      tooltip={isWaitingToBuy ? "在購物車移除商品" : "加入購物車"}
+      tooltip={isInCart ? "在購物車移除商品" : "加入購物車"}
       activeClass="border-success text-success"
     />
-    // <button className="icon-btn">
-    //   <CartIcon title="放入購物車" strokeWidth={1} />
-    // </button>
   );
 }
 
