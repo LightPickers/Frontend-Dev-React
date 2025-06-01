c4157348fc94fb897bc8b875f7e7158ea4ed23cd
zxlee0114 <napoleon.lee0114@gmail.com>
2025-06-02 01:25:09 +0800

    feat (ScrollToTop): 將自動捲動上方改為元件，並新增條件邏輯

--- src/LightPickersApp.jsx
+++ src/LightPickersApp.jsx
@@ -1,28 +1,10 @@
-import { Outlet, useLocation } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { Outlet } from "react-router-dom";
 
 import Header from "@layouts/Header";
 import Footer from "@layouts/Footer";
-import { userApi } from "@features/users/userApi";
-
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
+import ScrollToTop from "@components/ScrollToTop";
 
 function LightPickersApp() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(userApi.endpoints.verifyAuth.initiate());
-  }, [dispatch]);
-
   return (
     <>
       <ScrollToTop />
