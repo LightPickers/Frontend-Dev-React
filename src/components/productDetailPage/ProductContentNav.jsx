66eba51e3d1b70f3dbca0c4c130ed17a436b7f86
zxlee0114 <napoleon.lee0114@gmail.com>
2025-06-02 01:27:10 +0800

    feat: 新增 ProductContentNav 元件

---
+++ src/components/productDetailPage/ProductContentNav.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+import { string } from "prop-types";
+
+import { TextMedium } from "@components/TextTypography";
+
+function ProductContentNav({ id }) {
+  const PRODUCT_PATH = `/products/${id}`;
+  const navItems = [
+    { label: "商品介紹", path: "" },
+    { label: "詳細規格", path: `${PRODUCT_PATH}/specification` },
+    { label: "會員評價", path: `${PRODUCT_PATH}/review` },
+  ];
+  return (
+    <nav className="product-nav">
+      <ul className="ps-0 mb-0 d-flex align-items-center">
+        {navItems.map((item, index) => {
+          const { label, path } = item;
+          return (
+            <li key={index} className="nav-separator">
+              <TextMedium as={NavLink} end to={path}>
+                {label}
+              </TextMedium>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+ProductContentNav.propTypes = {
+  id: string.isRequired,
+};
+
+export default ProductContentNav;
