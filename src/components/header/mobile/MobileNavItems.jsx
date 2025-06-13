de5db75f67321619282130733f2dba5e42faea4d
zxlee0114 <napoleon.lee0114@gmail.com>
2025-06-13 22:20:00 +0800

    chore: 刪除不必要的引入

--- src/components/header/mobile/MobileNavItems.jsx
+++ src/components/header/mobile/MobileNavItems.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { func } from "prop-types";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import classNames from "classnames";
 
 import { TextLarge, TextMedium } from "@/components/TextTypography";
