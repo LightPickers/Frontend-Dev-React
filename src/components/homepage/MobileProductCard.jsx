7c2a2828c59d581d1451ebbb60e88ef962b19f77
zxlee0114 <napoleon.lee0114@gmail.com>
2025-06-14 23:54:32 +0800

    feat: (MobileProductCard) 新增手機板商品卡片

---
+++ src/components/homepage/MobileProductCard.jsx
@@ -0,0 +1,37 @@
+import { object } from "prop-types";
+import { Link } from "react-router-dom";
+
+import { H6Secondary } from "@components/Headings";
+import { TextSmall, TextMedium, LabelText } from "@components/TextTypography";
+import { formatPrice } from "@utils/formatPrice";
+
+function MobileProductCard({ product }) {
+  const { id, name, condition, primary_image, original_price, selling_price } = product;
+  return (
+    <main className="mobile-product-card">
+      <section className="img-container">
+        <img src={primary_image} alt={name} />
+        <LabelText className="card-badge mt-3">{condition}</LabelText>
+      </section>
+      <section className="card-content d-flex flex-column gap-2">
+        <H6Secondary isBold={false} className="text-balance line-clamp-2">
+          {name}
+        </H6Secondary>
+        <div className="product-price mt-auto">
+          <TextSmall className="line-through text-gray-400">{`NT$ ${formatPrice(original_price, false)}`}</TextSmall>
+          <p className="d-flex align-items-end gap-1 text-primary-1000">
+            <TextSmall>NT$</TextSmall>
+            <TextMedium>{formatPrice(selling_price, false)}</TextMedium>
+          </p>
+        </div>
+      </section>
+      <Link to={`/products/${id}`} className="stretched-link" title={`前往查看 ${name}`} />
+    </main>
+  );
+}
+
+MobileProductCard.propTypes = {
+  product: object.isRequired,
+};
+
+export default MobileProductCard;
