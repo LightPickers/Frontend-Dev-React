7bd2a08cd3b4abf219803d152fcf6b1654836484
zxlee0114 <napoleon.lee0114@gmail.com>
2025-06-21 20:16:10 +0800

    feat: 調整 alert 樣式

--- src/components/Alerts.jsx
+++ src/components/Alerts.jsx
@@ -1,29 +1,38 @@
-import { default as Swal } from "sweetalert2";
-import { default as withReactContent } from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const LP_Swal = withReactContent(Swal);
 
 // 確認對話框
-export const ConfirmAlert = ({ title, text, icon = "warning" }) => {
+export const ConfirmAlert = ({
+  title,
+  text,
+  icon = "warning",
+  confirmText = "確認",
+  cancelText = "取消",
+}) => {
   return LP_Swal.fire({
     title: <h3 className="alert-title">{title}</h3>,
     text: text,
     icon: icon,
     showCancelButton: true,
-    cancelButtonText: "取消",
-    confirmButtonText: "確認",
+    cancelButtonText: cancelText,
+    confirmButtonText: confirmText,
     customClass: {
       popup: "alert-popup",
       cancelButton: "alert-cancel-button",
       confirmButton: "alert-confirm-button",
       icon: "alert-icon",
     },
+    background: "rgba(255, 255, 255, 0.6)",
     buttonsStyling: false,
   });
 };
 
 /** @type {(title: string, text: string, icon: string, action: () => void | Promise<void>) => Promise<void>} */
+
 // ConfirmDialogue({title: "", text: "", icon: "", action: () => {}})
+// sweetAlert icon 選項："success"、"error"、"warning"、"info"、"question"
 export const ConfirmDialogue = async ({ title, text, icon = "warning", action }) => {
   const result = await ConfirmAlert({ title, text, icon });
   if (result.isConfirmed) {
@@ -37,59 +46,55 @@ export const ConfirmDialogue = async ({ title, text, icon = "warning", action })
 
 // 成功提示框
 // SuccessAlert({title: "", text: ""})
-export const SuccessAlert = ({ title = "成功！", text }) => {
+export const SuccessAlert = ({ title = "成功！", text, confirmText = "確定" }) => {
   return LP_Swal.fire({
-    title: <h3>{title}</h3>,
+    title: <h3 className="alert-title">{title}</h3>,
     text: text,
     icon: "success",
-    confirmButtonText: "確定",
+    confirmButtonText: confirmText,
     customClass: {
       popup: "alert-popup",
       confirmButton: "alert-success-button",
       icon: "alert-icon",
     },
+    // background: "rgba(250, 255, 248, 0.6)",
+    background: "rgba(255, 255, 255, 0.6)",
     buttonsStyling: false,
   });
 };
 
 // 資訊提示框
 // InfoAlert({title: "", html: ""})
-export const InfoAlert = ({ title = "提示", html }) => {
+export const InfoAlert = ({ title = "提示", html, confirmText = "我知道了" }) => {
   return LP_Swal.fire({
-    title: <h3>{title}</h3>,
+    title: <h3 className="alert-title">{title}</h3>,
     html,
     icon: "info",
-    confirmButtonText: "我知道了",
+    confirmButtonText: confirmText,
     customClass: {
       popup: "alert-popup",
       confirmButton: "alert-info-button",
       icon: "alert-icon",
     },
+    background: "rgba(255, 255, 255, 0.6)",
     buttonsStyling: false,
   });
 };
 
 // 錯誤提示框
 // ErrorAlert({title: "", text: ""})
-export const ErrorAlert = ({ title = "錯誤！", text }) => {
+export const ErrorAlert = ({ title = "錯誤！", text, confirmText = "確定" }) => {
   return LP_Swal.fire({
-    title: <h3>{title}</h3>,
+    title: <h3 className="alert-title">{title}</h3>,
     text: text,
     icon: "error",
-    confirmButtonText: "確定",
+    confirmButtonText: confirmText,
     customClass: {
       popup: "alert-popup",
       confirmButton: "alert-error-button",
       icon: "alert-icon",
     },
+    background: "rgba(255, 255, 255, 0.6)",
     buttonsStyling: false,
   });
-};
-
-export default {
-  ConfirmAlert,
-  ConfirmDialogue,
-  SuccessAlert,
-  ErrorAlert,
-  InfoAlert,
 };
